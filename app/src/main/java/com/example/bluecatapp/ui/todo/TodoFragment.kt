836fb1c5ee7eb345fce4a0c73560b4806bbcfaf4
bluecandle <jinyeongkim37@gmail.com>
2019-11-02 16:01:35 +0900

    Homework mode turn on/off dialog added.

--- app/src/main/java/com/example/bluecatapp/ui/todo/TodoFragment.kt
+++ app/src/main/java/com/example/bluecatapp/ui/todo/TodoFragment.kt
@@ -16,8 +16,17 @@ import com.example.bluecatapp.AddTodoActivity
 import com.example.bluecatapp.R
 import com.example.bluecatapp.data.TodoItem
 import kotlinx.android.synthetic.main.fragment_todo.*
+import android.content.DialogInterface
+import android.text.Editable
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.example.bluecatapp.MainActivity
 
-class TodoFragment : Fragment(){
+
+class TodoFragment : Fragment() {
+
+    val hwDoneDisplayText = "I have done my homework."
+    val hwDoneConfirmText = hwDoneDisplayText.replace("\\s".toRegex(), "").toLowerCase()
 
     private val ADD_TODO_REQUEST = 1
     private lateinit var todoViewModel: TodoViewModel
@@ -26,7 +35,8 @@ class TodoFragment : Fragment(){
 
     private var isHomeworkMode = false;
 
-    private var hwModeTime = 1000*60*45 // 45 minutes @todo should be changed as set on the settings page
+    private var hwModeTime =
+        1000 * 60 * 45 // 45 minutes @todo should be changed as set on the settings page
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -67,8 +77,8 @@ class TodoFragment : Fragment(){
         }
 
         // view model 을 통해 특정 todoItem 에 대한 데이터 변경 실행.
-        todoAdapter.onItemClick = {todoItem ->
-//            Log.d("test","todoItem test "+todoItem.id.toString()+"/"+todoItem.done.toString())
+        todoAdapter.onItemClick = { todoItem ->
+            //            Log.d("test","todoItem test "+todoItem.id.toString()+"/"+todoItem.done.toString())
             todoViewModel.updateTodoStatus(todoItem)
         }
 
@@ -81,42 +91,44 @@ class TodoFragment : Fragment(){
             setHasFixedSize(true)
         }
 
-        todoAdapter2.onItemClick = {todoItem ->
-//            Log.d("test","todoItem test "+todoItem.id.toString()+"/"+todoItem.done.toString())
+        todoAdapter2.onItemClick = { todoItem ->
+            //            Log.d("test","todoItem test "+todoItem.id.toString()+"/"+todoItem.done.toString())
             todoViewModel.updateTodoStatus(todoItem)
         }
 
-        container_hwmode.setOnClickListener{view->
-            if(isHomeworkMode){
-                view_timer.isCountDown = false
-                view_timer.base = SystemClock.elapsedRealtime() + hwModeTime // 45 minutes
-                view_timer.stop()
+        container_hwmode.setOnClickListener(clickListener)
 
-                isHomeworkMode = false;
-                text_homework.text = "Turn Homework Mode ON"
-                text_homework.setBackgroundColor(Color.parseColor("#111111"))
-                text_homework.setTextColor(Color.parseColor("#ffffff"))
-                todo_ll_container.setBackgroundColor(Color.parseColor("#ffffff"))
-                clock_homework.visibility = View.VISIBLE
-//                text_hw_timer.visibility = View.GONE
-                view_timer.visibility = View.GONE
-
-            }else{
-                view_timer.isCountDown = true
-                view_timer.base = SystemClock.elapsedRealtime() + hwModeTime
-                view_timer.start()
-
-                isHomeworkMode = true
-                text_homework.text = "Turn Homework Mode OFF"
-                text_homework.setBackgroundColor(Color.parseColor("#dddddd"))
-                text_homework.setTextColor(Color.parseColor("#111111"))
-                todo_ll_container.setBackgroundColor(Color.parseColor("#111111"))
-                clock_homework.visibility = View.GONE
-//                text_hw_timer.visibility = View.VISIBLE
-                view_timer.visibility = View.VISIBLE
-
-            }
-        }
+//        container_hwmode.setOnClickListener { view ->
+//            if (isHomeworkMode) {
+//                view_timer.isCountDown = false
+//                view_timer.base = SystemClock.elapsedRealtime() + hwModeTime // 45 minutes
+//                view_timer.stop()
+//
+//                isHomeworkMode = false;
+//                text_homework.text = "Turn Homework Mode ON"
+//                text_homework.setBackgroundColor(Color.parseColor("#111111"))
+//                text_homework.setTextColor(Color.parseColor("#ffffff"))
+//                todo_ll_container.setBackgroundColor(Color.parseColor("#ffffff"))
+//                clock_homework.visibility = View.VISIBLE
+////                text_hw_timer.visibility = View.GONE
+//                view_timer.visibility = View.GONE
+//
+//            } else {
+//                view_timer.isCountDown = true
+//                view_timer.base = SystemClock.elapsedRealtime() + hwModeTime
+//                view_timer.start()
+//
+//                isHomeworkMode = true
+//                text_homework.text = "Turn Homework Mode OFF"
+//                text_homework.setBackgroundColor(Color.parseColor("#dddddd"))
+//                text_homework.setTextColor(Color.parseColor("#111111"))
+//                todo_ll_container.setBackgroundColor(Color.parseColor("#111111"))
+//                clock_homework.visibility = View.GONE
+////                text_hw_timer.visibility = View.VISIBLE
+//                view_timer.visibility = View.VISIBLE
+//
+//            }
+//        }
 
         todo_add_task.setOnClickListener { view ->
             //            getActivity().
@@ -125,8 +137,74 @@ class TodoFragment : Fragment(){
                 ADD_TODO_REQUEST
             )
         }
-
     }
+
+    private val clickListener: View.OnClickListener = View.OnClickListener { view ->
+        when (view.id) {
+            R.id.container_hwmode -> {
+
+                if (isHomeworkMode) {
+
+                    var alertDialog = AlertDialog.Builder(requireContext()).create()
+                    val editText = EditText(requireContext())
+                    alertDialog.setMessage("Have you finished your homework?\n"+
+                            "Type in\n"+
+                            "\""+hwDoneDisplayText+"\"")
+                    alertDialog.setTitle("Turn Off HW Mode")
+
+                    alertDialog.setView(editText)
+
+                    alertDialog.setButton(DialogInterface.BUTTON_POSITIVE,"Yes!", DialogInterface.OnClickListener { dialog, whichButton ->
+
+                        val inputText = editText.text.toString().replace("\\s".toRegex(), "").toLowerCase()
+                        if(inputText.equals(hwDoneConfirmText)){
+                            view_timer.isCountDown = false
+                            view_timer.base = SystemClock.elapsedRealtime() + hwModeTime // 45 minutes
+                            view_timer.stop()
+
+                            isHomeworkMode = false;
+                            text_homework.text = "Turn Homework Mode ON"
+                            text_homework.setBackgroundColor(Color.parseColor("#111111"))
+                            text_homework.setTextColor(Color.parseColor("#ffffff"))
+                            todo_ll_container.setBackgroundColor(Color.parseColor("#ffffff"))
+                            clock_homework.visibility = View.VISIBLE
+//                text_hw_timer.visibility = View.GONE
+                            view_timer.visibility = View.GONE
+
+                            Toast.makeText(requireContext(),"Nice job!",Toast.LENGTH_LONG)
+                            alertDialog.dismiss()
+                        }else{
+                            Toast.makeText(requireContext(),"Type in the given sentence properly",Toast.LENGTH_LONG)
+                        }
+                    })
+
+                    alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE,"No...", DialogInterface.OnClickListener { dialog, whichButton ->
+//                        val inputText = editText.text.toString()
+                        Toast.makeText(requireContext(),"Keep Working!",Toast.LENGTH_LONG)
+                        alertDialog.dismiss()
+                    })
+
+                    alertDialog.show()
+
+                } else {
+                    Toast.makeText(requireContext(), "Good Luck!", Toast.LENGTH_LONG).show()
+                    view_timer.isCountDown = true
+                    view_timer.base = SystemClock.elapsedRealtime() + hwModeTime
+                    view_timer.start()
+
+                    isHomeworkMode = true
+                    text_homework.text = "Turn Homework Mode OFF"
+                    text_homework.setBackgroundColor(Color.parseColor("#dddddd"))
+                    text_homework.setTextColor(Color.parseColor("#111111"))
+                    todo_ll_container.setBackgroundColor(Color.parseColor("#111111"))
+                    clock_homework.visibility = View.GONE
+//                text_hw_timer.visibility = View.VISIBLE
+                    view_timer.visibility = View.VISIBLE
+                }
+            }
+        }
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.todo_menu, menu)
@@ -137,8 +215,7 @@ class TodoFragment : Fragment(){
         return when (item?.itemId) {
             R.id.todo_delete_all_tasks -> {
                 todoViewModel.deleteAllTodoItems()
-                Toast.makeText(requireContext(), "All To-do Items deleted!", Toast.LENGTH_SHORT)
-                    .show()
+                Toast.makeText(requireContext(), "All To-do Items deleted!", Toast.LENGTH_SHORT).show()
                 true
             }
             else -> {
